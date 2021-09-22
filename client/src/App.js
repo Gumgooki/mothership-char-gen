88c9017be4d1a3c3eaf4cf938d5a1900e5b5da38
Thomas Bunch <17713462+Gumgooki@users.noreply.github.com>
2021-09-21 19:41:45 -0500

    installed Material UI and played around to learn a bit on the homescreen.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
+import MotherOS from "./MotherOS.png";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+
 import "./App.css";
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+        <Box
+          sx={{
+            width: 400,
+            height: 500,
+            bgcolor: "primary.light",
+          }}
+        >
+          <img src={MotherOS} className="App-logo" alt="logo" />
+          <p>MotherOS</p>
+          <Button variant="contained">Click Here To Enter</Button>
+        </Box>
       </header>
     </div>
   );
